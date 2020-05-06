592f6be65d646e7f2b4931639188750a73889afc
Alan <tonyyxm@qq.com>
2020-05-06 15:44:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## 弹性滚动组件
+
+
+### OverScrollListView
+
+ListView 子类，同ListView用法
+
+
+### OverScrollView
+
+ScrollView 子类，同 ScrollView 用法
---
+++ lib/src/main/java/com/alan/lib/over/OnOverScrollListener.java
@@ -0,0 +1,12 @@
+package com.alan.lib.over;
+
+/**
+ * Created by changyou on 2015/11/11.
+ */
+public interface OnOverScrollListener {
+
+    void onTopOverScoll();
+
+    void onBottomOverScroll();
+
+}
